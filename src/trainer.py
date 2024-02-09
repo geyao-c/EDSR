e39fb1ff33c54ea77740134dce147fd2c65891f1
geyao-c <1279846803@qq.com>
2024-02-09 11:10:30 +0800

    xiugai

--- src/trainer.py
+++ src/trainer.py
@@ -20,9 +20,9 @@ class Trainer():
         self.loss = my_loss
         self.optimizer = utility.make_optimizer(args, self.model)
 
-        # if self.args.load != '':
-        #     print('ckp.dir: {}, len(ckp.log): {}'.format(ckp.dir, len(ckp.log)))
-        #     self.optimizer.load(ckp.dir, epoch=len(ckp.log))
+        if self.args.load != '':
+            print('ckp.dir: {}, len(ckp.log): {}'.format(ckp.dir, len(ckp.log)))
+            self.optimizer.load(ckp.dir, epoch=len(ckp.log))
 
         self.error_last = 1e8
 
