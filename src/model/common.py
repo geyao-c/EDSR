cd03436f6fe2e33e0377789dc7bec30a8ef83bed
geyao-c <1279846803@qq.com>
2024-02-09 20:13:42 +0800

    xiugai

--- src/model/common.py
+++ src/model/common.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# from model import ecb
+from model.ecb import ECB
+
 def default_conv(in_channels, out_channels, kernel_size, bias=True):
     return nn.Conv2d(
         in_channels, out_channels, kernel_size,
@@ -57,6 +60,30 @@ class ResBlock(nn.Module):
 
         return res
 
+class ECBResBlock(nn.Module):
+    def __init__(
+        self, conv, n_feats, kernel_size,
+        bias=True, bn=False, act=nn.ReLU(True), res_scale=1):
+
+        super(ECBResBlock, self).__init__()
+        m = []
+        for i in range(2):
+            # m.append(conv(n_feats, n_feats, kernel_size, bias=bias))
+            m.append(ECB(n_feats, n_feats, depth_multiplier=2.0, act_type='linear'))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if i == 0:
+                m.append(act)
+
+        self.body = nn.Sequential(*m)
+        self.res_scale = res_scale
+
+    def forward(self, x):
+        res = self.body(x).mul(self.res_scale)
+        res += x
+
+        return res
+
 class Upsampler(nn.Sequential):
     def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
 
@@ -85,4 +112,35 @@ class Upsampler(nn.Sequential):
             raise NotImplementedError
 
         super(Upsampler, self).__init__(*m)
+
+class ECBUpsampler(nn.Sequential):
+    def __init__(self, conv, scale, n_feats, bn=False, act=False, bias=True):
+
+        m = []
+        if (scale & (scale - 1)) == 0:    # Is scale = 2^n?
+            for _ in range(int(math.log(scale, 2))):
+                # m.append(conv(n_feats, 4 * n_feats, 3, bias))
+                m.append(ECB(n_feats, 4 * n_feats, depth_multiplier=2.0, act_type='linear'))
+                m.append(nn.PixelShuffle(2))
+                if bn:
+                    m.append(nn.BatchNorm2d(n_feats))
+                if act == 'relu':
+                    m.append(nn.ReLU(True))
+                elif act == 'prelu':
+                    m.append(nn.PReLU(n_feats))
+
+        elif scale == 3:
+            # m.append(conv(n_feats, 9 * n_feats, 3, bias))
+            m.append(ECB(n_feats, 9 * n_feats, depth_multiplier=2.0, act_type='linear'))
+            m.append(nn.PixelShuffle(3))
+            if bn:
+                m.append(nn.BatchNorm2d(n_feats))
+            if act == 'relu':
+                m.append(nn.ReLU(True))
+            elif act == 'prelu':
+                m.append(nn.PReLU(n_feats))
+        else:
+            raise NotImplementedError
+
+        super(ECBUpsampler, self).__init__(*m)
 
