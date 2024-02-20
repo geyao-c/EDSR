01a8b5dfc1af0cc105c0bc1f21d654339bbe1b61
geyao-c <1279846803@qq.com>
2024-02-20 11:53:43 +0800

    xiugai

--- src/model/common.py
+++ src/model/common.py
@@ -67,6 +67,7 @@ class ResBlock(nn.Module):
                 m.append(nn.BatchNorm2d(n_feats))
             if i == 0:
                 m.append(act)
+                m.append(se_module.SELayer(n_feats))
         m.append(se_module.SELayer(n_feats))
         self.body = nn.Sequential(*m)
         self.res_scale = res_scale
